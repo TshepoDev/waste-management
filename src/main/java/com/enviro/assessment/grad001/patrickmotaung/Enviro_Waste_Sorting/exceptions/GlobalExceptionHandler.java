8d7bf6c1e29e6d3b55964abfd89f9dfa758aed31
TshepoDev <pmotaung247@gmail.com>
2025-01-27 20:07:43 +0200

    GlobalExceptionHandler Update

--- src/main/java/com/enviro/assessment/grad001/patrickmotaung/Enviro_Waste_Sorting/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/patrickmotaung/Enviro_Waste_Sorting/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(
                 String.format("%s with ID %d not found", ex.getResourceName(), ex.getResourceId()),
@@ -29,7 +30,7 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(Exception.class)
-
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
 
@@ -37,6 +38,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
         // Group field errors by field name and collect all messages
         Map<String, List<String>> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
