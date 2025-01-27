963d02325454de496f2fd3ea71412c315e0879c2
TshepoDev <pmotaung247@gmail.com>
2025-01-27 20:46:26 +0200

    OpenAi Swagger Integration

--- src/main/java/com/enviro/assessment/grad001/patrickmotaung/Enviro_Waste_Sorting/EnviroWasteSortingApplication.java
+++ src/main/java/com/enviro/assessment/grad001/patrickmotaung/Enviro_Waste_Sorting/EnviroWasteSortingApplication.java
@@ -1,9 +1,12 @@
 package com.enviro.assessment.grad001.patrickmotaung.Enviro_Waste_Sorting;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Enviro Waste Sorting API", version = "v1", description = "API documentation for enviro waste sorting application"))
 public class EnviroWasteSortingApplication {
 
 	public static void main(String[] args) {
